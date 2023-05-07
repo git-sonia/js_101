bd670e275447508587f6b21a4169c481c7350896
Sonia <tsonia5987@gmail.com>
2023-05-06 22:00:44 -0700

    psuedocode practice

---
+++ lesson_2/psuedocode.js
@@ -0,0 +1,11 @@
+//SET new array
+//SET index of first array to 0
+//SET index of second array to 0
+//WHILE both indices are within range
+  //push element at the index of first array to new array
+  //push element at index of second array
+  //increment both indicies
+//return new array
+
+merge([1, 2, 3], [4, 5, 6]); // => [1, 4, 2, 5, 3, 6]
+
