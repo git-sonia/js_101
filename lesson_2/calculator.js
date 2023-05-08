30a3f4598b6c256fd0d7b080da9d18dd28dd77ee
Sonia <tsonia5987@gmail.com>
2023-05-07 22:14:04 -0700

    added prompt

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,16 +1,20 @@
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
 const readline = require('readline-sync');
 
-console.log('Welcome to Calculator!');
+prompt('Welcome to Calculator!');
 
-console.log("What's the first number?");
+prompt("What's the first number?");
 let number1 = readline.question();
 
-console.log("What's the second number?");
+prompt("What's the second number?");
 let number2 = readline.question();
 
-console.log(`${number1} ${number2}`);
+prompt(`${number1} ${number2}`);
 
-console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
 
 let output;
@@ -24,4 +28,4 @@ if (operation === '1') { // '1' represents addition
   output = Number(number1) / Number(number2);
 }
 
-console.log(`The result is: ${output}`);
+prompt(`The result is: ${output}`);
