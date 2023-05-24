67b2ad7a7db900bcab933ad98f7424139a590262
Sonia <tsonia5987@gmail.com>
2023-05-23 20:49:20 -0700

    added best of five

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,39 +1,78 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const ROUNDS = 3;
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
-
+function findWinner(choice, computerChoice) {
   if ((choice === 'rock' && computerChoice === 'scissors') ||
     (choice === 'paper' && computerChoice === 'rock') ||
     (choice === 'scissors' && computerChoice === 'paper')) {
-    prompt('You win!');
+    return 'player';
   } else if ((choice === 'rock' && computerChoice === 'paper') ||
     (choice === 'paper' && computerChoice === 'scissors') ||
     (choice === 'scissors' && computerChoice === 'rock')) {
-    prompt('Computer wins!');
+    return 'computer';
   } else {
-    prompt("It's a tie!");
+    return 'tie';
   }
 }
 
-while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
-
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
+function displayWinner(winResult, choice, computerChoice, points) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+  switch (winResult) {
+    case 'player':
+      prompt('You win!');
+      break;
+    case 'computer':
+      prompt('Computer wins!');
+      break;
+    default:
+      prompt("It's a tie!");
   }
+  prompt(`player: ${points['player']}, computer: ${points['computer']}`);
+}
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+function updatePoints(winResult, points) {
+  if (winResult !== 'tie') {
+    points[winResult] += 1;
+  }
+}
 
-  displayWinner(choice, computerChoice);
+function findOverallWinner(points) {
+  let mostPoints = 0;
+  let winner;
+  for (let player in points) {
+    if (points[player] > mostPoints) {
+      mostPoints = points[player];
+      winner = player;
+    }
+  }
+  return winner;
+}
+
+while (true) {
+  let points = { player: 0, computer: 0 };
+  while (points['player'] < ROUNDS && points['computer'] < ROUNDS) {
+    prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+    let choice = readline.question();
+
+    while (!VALID_CHOICES.includes(choice)) {
+      prompt("That's not a valid choice");
+      choice = readline.question();
+    }
+
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
+
+    let winResult = findWinner(choice, computerChoice);
+    updatePoints(winResult, points);
+    displayWinner(winResult, choice, computerChoice, points);
+  }
+  let overallWinner = findOverallWinner(points);
+  prompt(`Overall winner is ${overallWinner} with ${points[overallWinner]} points!`);
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
